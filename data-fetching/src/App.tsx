176be72e49688c0f1654ec4477abe458939e4bbc
dylanjohn <dylan@mediamachine.co.za>
2025-12-07 16:01:02 +0200

    data fetching

---
+++ data-fetching/src/App.tsx
@@ -0,0 +1,51 @@
+import { type ReactNode, useEffect, useState } from 'react';
+
+import BlogPosts, { BlogPost } from './components/BlogPosts';
+import { get } from './utils/http';
+import fetchingImg from './assets/data-fetching.png';
+
+type RawDataBlogPost = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+function App() {
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const data = (await get(
+        'https://jsonplaceholder.typicode.com/posts',
+      )) as RawDataBlogPost[];
+
+      const blogPosts: BlogPost[] = data.map((rawPost) => {
+        return {
+          id: rawPost.id,
+          title: rawPost.title,
+          text: rawPost.body,
+        };
+      });
+
+      setFetchedPosts(blogPosts);
+    }
+
+    fetchPosts();
+  }, []);
+
+  let content: ReactNode;
+
+  if (fetchedPosts) {
+    content = <BlogPosts posts={fetchedPosts} />; // Assign to content
+  }
+
+  return (
+    <main>
+      <img src={fetchingImg} alt="Logo" />
+      {content}
+    </main>
+  );
+}
+
+export default App;
